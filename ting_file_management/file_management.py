e75688bb296269159f18b27923fa9183acc3c5a6
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-04-10 18:27:31 -0500

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação. Glória ao nome de Jesus!"""
+    path = path_file.split('.')
+    if path[1] != 'txt':
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, mode='r') as file:
+            file_lines = file.read().split('\n')
+            return file_lines
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+
+print(txt_importer('matheus.txt'))
