f2fabb26ec8d7a8b391c397e067d8af2a73196e1
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-04-12 13:11:01 -0500

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    # 1 - vou passar por cada instanciação, que nesse caso
+    # é cada "arquivo" lido
+    # 2 - agora vamos passar por cada linha e verificar se há
+    # a palavra procurada.
+    data = []
+    for index in range(len(instance)):
+        occurences = []
+        dict_lines = instance.search(index)
+        for i, line in enumerate(dict_lines["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurences.append({"linha": i + 1})
+        if occurences:
+            data.append({
+                "palavra": word,
+                "arquivo": dict_lines["nome_do_arquivo"],
+                "ocorrencias": occurences
+            })
+    return data
 
 
 def search_by_word(word, instance):
